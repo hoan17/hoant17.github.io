8fca444a93d44f4be4a269dc69f192b4438f5e61
hoant626 <hoant626@newschool.edu>
2021-04-02 11:44:16 -0400

    where can you find test

--- thesis/where-can-you-find-it/js/main.js
+++ thesis/where-can-you-find-it/js/main.js
@@ -17,5 +17,43 @@ $(document).ready(function() {
     }
   });
 
+//Hover
+
+  $( ".item1" ).mouseover(function() {
+    $( ".text1" ).addClass( "showcap" );
+  });
+  $( ".item1" ).mouseout(function() {
+    $( ".text1" ).removeClass( "showcap" );
+  });
+
+  $( ".item2" ).mouseover(function() {
+    $( ".text2" ).addClass( "showcap" );
+    $(".map-img").attr("src", "style/svg/map-easter.svg");
+  });
+  $( ".item2" ).mouseout(function() {
+    $( ".text2" ).removeClass( "showcap" );
+    $(".map-img").attr("src", "style/svg/map-base.svg");
+  });
+
+  $( ".item3" ).mouseover(function() {
+    $( ".text3" ).addClass( "showcap" );
+  });
+  $( ".item3" ).mouseout(function() {
+    $( ".text3" ).removeClass( "showcap" );
+  });
+
+  $( ".item4" ).mouseover(function() {
+    $( ".text4" ).addClass( "showcap" );
+  });
+  $( ".item4" ).mouseout(function() {
+    $( ".text4" ).removeClass( "showcap" );
+  });
+
+  $( ".item5" ).mouseover(function() {
+    $( ".text5" ).addClass( "showcap" );
+  });
+  $( ".item5" ).mouseout(function() {
+    $( ".text5" ).removeClass( "showcap" );
+  });
 
 });
